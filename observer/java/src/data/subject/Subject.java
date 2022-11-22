1fc0e3af02fe95b728d8c8f7642071585aa15d64
Claudio D. Sanchez <lmhj.luijo@gmail.com>
2022-11-22 00:07:59 -0400

    feat(observable): :sparkles: add observable pattern imp

---
+++ observer/java/src/data/subject/Subject.java
@@ -0,0 +1,9 @@
+package data.subject;
+
+import data.observer.Observer;
+
+public interface Subject {
+    public void registerObserver(Observer o);
+    public void removeObserver(Observer o);
+    public void notifyObservers();
+}
